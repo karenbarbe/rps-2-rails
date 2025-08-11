a6f93c1e757aec395a40af186864edc4fb163242
Karen Barbé <info@karenbarbe.com>
2025-08-11 14:56:35 -0500

    Create pages_controller.rb

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,50 @@
+class PagesController < ApplicationController
+
+  @moves = ["rock", "paper", "scissors"]
+
+  def rules
+    render({ :template => "game_templates/rules"})
+  end
+
+  def rock
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "tied"
+    elsif @computer_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+  end
+
+    render({ :template => "game_templates/play_rock"})
+  end
+
+  def paper
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "won"
+    elsif @computer_move == "paper"
+      @outcome = "tied"
+    else
+      @outcome = "lost"
+  end
+
+    render({ :template => "game_templates/play_paper"})
+  end
+
+  def scissors
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "lost"
+    elsif @computer_move == "paper"
+      @outcome = "won"
+    else
+      @outcome = "tied"
+  end
+
+    render({ :template => "game_templates/play_scissors"})
+  end
+end
